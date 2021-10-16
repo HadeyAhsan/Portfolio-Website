53788b8769700fc2bc93f4ec33e072c4ab6e249f
Hadi Ahsan <hadiahsan@Hadis-MacBook-Air.local>
2021-10-16 17:32:31 -0400

    Updates

---
+++ precache-manifest.06dfe38e8d1f5e928cb43378cd1ab231.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c35dfc249b73b88a54e30b3731d084e1",
+    "url": "/PortfolioWebsite/index.html"
+  },
+  {
+    "revision": "5c2b9a85562fb981cd08",
+    "url": "/PortfolioWebsite/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "ef6b9de55daea6767017",
+    "url": "/PortfolioWebsite/static/css/main.db6f0745.chunk.css"
+  },
+  {
+    "revision": "5c2b9a85562fb981cd08",
+    "url": "/PortfolioWebsite/static/js/2.88969ec0.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/PortfolioWebsite/static/js/2.88969ec0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef6b9de55daea6767017",
+    "url": "/PortfolioWebsite/static/js/main.27073399.chunk.js"
+  },
+  {
+    "revision": "3a6c9b069df2a43c5335",
+    "url": "/PortfolioWebsite/static/js/runtime-main.aa886001.js"
+  }
+]);
